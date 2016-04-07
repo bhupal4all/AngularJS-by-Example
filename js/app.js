158a2fe492dfeaa6c43724768adafca797cead89
Ranga Bhupal Reddy Gangavarapu <rgangavarapu@manh.com>
2016-04-07 19:59:44 +0530

    adding routing features to sample templates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 'use strict';
-angular.module('app', ['7minworkout']);
+
+angular.module('app', ['ngRoute','7minworkout'])
+	.config(function($routeProvider){
+		$routeProvider.when('/start', {
+			templateUrl: 'partials/start.xhtml'
+		});
+		$routeProvider.when('/workout', {
+			templateUrl: 'partials/workout.xhtml'
+		});
+		$routeProvider.when('/relax', {
+			templateUrl: 'partials/relax.xhtml'
+		});
+		$routeProvider.when('/end', {
+			templateUrl: 'partials/end.xhtml'
+		});
+
+		$routeProvider.otherwise({ redirectTo: '/start' });
+	});
+
+
 angular.module('7minworkout', []);
+
