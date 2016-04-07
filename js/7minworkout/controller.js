2a04be02b06111a4e2ecf7c151904c30a07abc0f
Ranga Bhupal Reddy Gangavarapu <rgangavarapu@manh.com>
2016-04-07 19:44:44 +0530

    adding images and incorporating the changes

--- js/7minworkout/controller.js
+++ js/7minworkout/controller.js
@@ -34,7 +34,8 @@ angular.module('7minworkout')
 					details: new Exercise({
 							name: 'Rest',
 							title: 'Relax !!!',
-							description: 'Take some rest'
+							description: 'Take some rest',
+			                image: "img/rest.png",							
 						}),
 					duration: workoutPlan.restBetweenExercise
 				};
@@ -46,25 +47,42 @@ angular.module('7minworkout')
 			var workout = new WorkoutPlan({
               name: "7minWorkout",
               title: "7 Minute Workout",
-              restBetweenExercise: 10
+              restBetweenExercise: 5
           });
 
-			workout.exercises.push({
-				details: new Exercise({
-					name: 'one',
-					title: 'Exercise 1',
-					description: 'First Exercise'
+		workout.exercises.push({
+			details: new Exercise({
+				name: "jumpingJacks",
+				title: "Jumping Jacks / Star Jumps",
+				description: "Notes: This exercise can be performed as a timed exercise, completing as many reps as possible in a set time or, \
+					in sets with a fixed number of repetitions per set.\
+					To increase intensity, bend your arms slightly as you raise them to engage your biceps and triceps and squeezing them during each rep.",
+				image: "img/JumpingJacks.png",
+				videos: ["//www.youtube.com/embed/dmYwZH_BNd0", "//www.youtube.com/embed/BABOdJ-2Z6o", "//www.youtube.com/embed/c4DAnQ6DtF8"],
+				procedure: "1. Stand with your feet together, arms fully extended with your hands by your sides. This is the start position.\
+					2. Bend your knees slightly then straighten and push through the balls of your feet while straightening your your \
+					knees to jump up spreading your legs to wider than hip width apart.\
+					3. As you do so, raise both arms out and up in a smooth arc until your hands meet above your head.\
+					4. As you return to the ground, bring your feet together and your hands back to your sides with your arms fully extended.\
+					5. Continue without pause for the desired amount of time or repetitions."
 				}),
 				duration: 10
 			});
-
-			workout.exercises.push({
-				details: new Exercise({
-					name: 'two',
-					title: 'Exercise 2',
-					description: 'Second Exercise'
+		
+		workout.exercises.push({
+			details: new Exercise({
+					name: "wallSit",
+					title: "Wall Sit",
+					description: "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
+					image: "img/wallsit.png",
+					videos: ["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
+					procedure: "1. Stand tall against a wall with your head and back touching the wall.\
+						2. Position your feet so that they are shoulder-width apart and a few inches away from the wall.\
+						3. Rest your arms at your sides.\
+						4. Bend your knees and lower into a squat position until your thighs are parallel to the floor and hold the position\
+						5. Return to starting position by straightening your knees and standing tall again."
 				}),
-				duration: 15
+				duration: 10
 			});
 
 			return workout;
